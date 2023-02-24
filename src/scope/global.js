eb05fa744b8fc80e63b275572b67013158b35e22
jsha64 <jorgemerino743@gmail.com>
2023-02-24 15:23:46 -0500

    prueba de practica

---
+++ src/scope/global.js
@@ -0,0 +1,24 @@
+var a; //declarando
+var b = 'b'; // declaramos / asignamos
+b = 'bb'; //reasignacion
+var a = 'aa'; //redeclaracion
+
+
+// Global Scope
+var fruit = 'Apple'; //global
+
+function bestFruit() {
+    console.log(fruit);
+}
+
+bestFruit();
+
+
+
+function countries() {
+    country = 'Colombia';
+    console.log(country);
+}
+countries();
+console.log(country);
+
