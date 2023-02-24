1f27badb823e14560b8bfb07bf5c60c525aedb7b
jsha64 <jorgemerino743@gmail.com>
2023-02-24 16:13:03 -0500

    render

---
+++ src/scope/re.js
@@ -0,0 +1,33 @@
+var firstName; // Undefined
+firstName = 'Oscar';
+console.log(firstName);
+
+var lastName = 'David'; // declarar / asignar
+lastName = 'Ana' //reasignar
+console.log(lastName);
+
+var secondName = 'David' //declarando / asignando 
+var secondName = 'Ana' // reasignando
+console.log(secondName);
+
+//let
+let fruit = 'Apple'; // declarar y asignar
+fruit = ' Kiwi'; // reasignar
+console.log(fruit);
+
+let fruit = 'Banana';
+console.log(fruit);
+
+//const
+const animal = 'dog'; // declara y asigna
+// animal = 'cat'; //reasignado
+const animal = 'Snake';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('BMW');
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
+
